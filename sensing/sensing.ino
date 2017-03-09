6e94e3cd989753b1b86b93e558bbe74f3b3a612d
Paul <paulerikf@gmail.com>
2017-03-09 21:31:02 +0100

    change sensing.ino to focus only on ultrasonic sensor
    We'll think about servos later

--- sensing/sensing.ino
+++ sensing/sensing.ino
@@ -1,48 +1,14 @@
-#include <Servo.h>
-Servo myservo;
-int pos = 0;
-const int echoPin = 7;
-const int triggerPin = 8;
- 
+#include "Ping.h"
+
+Ping ping(8, 7);
 
 void setup() {
   Serial.begin(9600);
-  
-  // Definer hvilken type de forskjellige pinnene er
-  pinMode(triggerPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  myservo.attach(9);
 }
 
 void loop() {
-  digitalWrite(triggerPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(triggerPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(triggerPin, LOW);
-  // Mål hvor lang tid det tar før ekkoet kommer tilbake
-  long duration = pulseIn(echoPin, HIGH);
-  // Regn ut avstanden
-  float distance = duration * 0.017;  
-   
+  float distance = ping.pulse();
   Serial.println(distance);
-  delay(150);
-  /*
-  myservo.write(0);
-  delay(1000);
-  myservo.write(90);
-  delay(1000);
-  myservo.write(180);
-  delay(1000);
-  myservo.write(90);
-  delay(1000);
-  for (pos = 0; pos <= 30; pos += 1) {
-    myservo.write(pos);
-    delay(50);
-  }
-  for (pos = 30; pos >= 0; pos -= 1) {
-    myservo.write(pos);
-    delay(50);
-  }
-  */
+  delay(100);
 }
+
