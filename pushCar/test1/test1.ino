e7f519f20de5f58f0c28f6da6dff159d3639cdc4
christholest <christ.holest@outlook.com>
2017-03-30 17:38:37 +0200

    added bluetooth handler code

--- pushCar/test1/test1.ino
+++ pushCar/test1/test1.ino
@@ -3,6 +3,8 @@ Drive forward or reverse when border is detected
   - Only reflectionsensor 0 and 5 are used.
   - Demonstrates use of flipLeftMotor and flipRightMotor
 */
+#include <SoftwareSerial.h>
+#include <PLabBTSerial.h>
 #include <ZumoMotors.h>
 #include <Pushbutton.h>
 #include <QTRSensors.h>
@@ -15,7 +17,8 @@ Drive forward or reverse when border is detected
 #define QTR_THRESHOLD  1500 // this might need to be tuned for different
                             //lighting conditions, surfaces, etc
 #define SPEED     200       //0 is stopped, 400 is full speed
- 
+
+
 
 ZumoMotors motors;
 Pushbutton button(ZUMO_BUTTON); // pushbutton on pin 12
@@ -36,16 +39,20 @@ NewPing sonar(triggerPin, echoPin, maxDistance);
 NewPing sonarL(triggerPinL, echoPinL, maxDistance);
 NewPing sonarR(triggerPinR, echoPinR, maxDistance);
 
+const int btUnitTxPin = 2; // Connected to tx on bt unit
+const int btUnitRxPin = 6; // Connected to rx on bt unit
+
  
 ZumoReflectanceSensorArray reflectanceSensors; //(QTR_NO_EMITTER_PIN);
 
-
+PLabBTSerial btSerial(btUnitTxPin, btUnitRxPin);
  
 void setup()
 {
    reflectanceSensors.init();
    button.waitForButton();
    Serial.begin(9600);
+   btSerial.begin(9600);
 
 }
 
@@ -98,5 +105,35 @@ void loop(){
     }*/
     else {
     motors.setSpeeds(SPEED, SPEED);
-    }    
+    }
+
+    // See if we have received a new character
+    int availableCount = btSerial.available();
+    if (availableCount > 0) {
+      char text[availableCount];
+      btSerial.read(text, availableCount); 
+      readCommand(text);
+    }
+}
+
+void readCommand (char *text) {
+  if (0 == strcmp("FORWARD", text)) {
+    PLab_motors.setSpeeds(200, 200);
+  } else if (0 == strcmp("STOP", text)) {
+    PLab_motors.setSpeeds(0, 0);
+  } else if (0 == strcmp("BACKWARD", text)) {
+    PLab_motors.setSpeeds(-200, -200);
+  } else if (0 == strcmp("LEFT", text)) {
+    PLab_motors.turnLeft(200, 90);
+  } else if (0 == strcmp("L45", text)) {
+   PLab_motors.turnLeft(200, 45);
+  } else if (0 == strcmp("RIGHT", text)) {
+    PLab_motors.turnRight(200, 90);
+  } else if (0 == strcmp("R45", text)) {
+   PLab_motors.turnRight(200, 45);  
+  } else if (0 == strcmp("STEPF", text)) {
+    PLab_motors.forward(200, 5);
+  } else if (0 == strcmp("STEPB", text)) {
+   PLab_motors.backward(200, 5);  
+  } 
 }
